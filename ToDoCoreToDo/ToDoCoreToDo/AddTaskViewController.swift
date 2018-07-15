596e5d005f53ad4e5485810bc188b98862ecec8a
TakJim <tak.jimbo@gmail.com>
2018-07-16 01:16:15 +0900

    データベースへの保存とtaskTableViewへの表示まで

--- ToDoCoreToDo/ToDoCoreToDo/AddTaskViewController.swift
+++ ToDoCoreToDo/ToDoCoreToDo/AddTaskViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class AddTaskViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBOutlet weak var categorySegmentedControl: UISegmentedControl!
+    
+    // MARK: -
+    var taskCategory = "TODO"
+    
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,6 +30,51 @@ class AddTaskViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    // MARK: - Actions of Buttons
+    
+    @IBAction func categoryChosen(_ sender: UISegmentedControl) {
+        // choose category of task
+        switch sender.selectedSegmentIndex{
+        case 0:
+            taskCategory = "TODO"
+        case 1:
+            taskCategory = "Shopping"
+        case 2:
+            taskCategory = "Assignment"
+        default:
+            taskCategory = "TODO"
+        }
+    }
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func plusButtonTapped(_ sender: Any) {
+        // TextFieldに何も入力されていない場合は何もせずに1つ目のビューに戻る
+        let taskName = taskTextField.text
+        if taskName == "" {
+            dismiss(animated: true, completion: nil)
+            return
+        }
+        // context（データベースを扱うのに必要）を定義
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        // taskにTask（データベースのエンティティ）型オブジェクトを代入する．
+        // このとき，Taskがサジェストされない（エラーになる）場合がある
+        // 詳しい原因はわからないが，Runするか，すべてのファイルを保存してXcodeを再起動すると直るのでいろいろ試す．
+        let task = Task(context: context)
+        
+        // 先ほど定義したTask型データのname, categoryプロパティに入力，選択したデータを代入
+        task.name = taskName
+        task.category = taskCategory
+        
+        // 上で作成したデータをデータベースに保存します
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
     
 
     /*
--- ToDoCoreToDo/ToDoCoreToDo/ViewController.swift
+++ ToDoCoreToDo/ToDoCoreToDo/ViewController.swift
@@ -7,23 +7,85 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     // MARK: - Properties
+    @IBOutlet weak var taskTableView: UITableView!
     
-    @IBOutlet weak var taksTableView: UITableView!
+    // MARK: - Properties for table view
     
-    // MARK: - View Life Cycle
+    var tasks:[Task] = []
+    var tasksToShow:[String:[String]] = ["TODO":[], "Shopping":[], "Assignment":[]]
+    let taskCategories:[String] = ["TODO", "Shopping", "Assignment"]
+    
+    // MARK: - Viwe Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        taskTableView.dataSource = self
+        taskTableView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // CoreDataからデータをfetchしてくる
+        getData()
+        
+        // taskTableViewを再読み込みする
+        taskTableView.reloadData()
+    }
+    
+    // MARK: - Method of Getting data from Core Data
+    
+    func getData() {
+        // データ保存時と同様にcontextを定義
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do {
+            // CoreDataからデータをfetchしてtasksに格納
+            let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+            tasks = try context.fetch(fetchRequest)
+            
+            // tasksToShow配列を初期化（同じデータを複数表示しないため）
+            for key in tasksToShow.keys {
+                tasksToShow[key] = []
+            }
+            //　先程fetchしたデータをtasksToShow配列に格納する
+            for task in tasks {
+                tasksToShow[task.category!]?.append(task.name!)
+            }
+        } catch {
+            print("Fetching Failed.")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table View Data Source
+    
+    // taskCategories[]に格納されている文字列がTableViweのセクションになる
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return taskCategories.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return tasksToShow[taskCategories[section]]!.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        let sectionData = tasksToShow[taskCategories[indexPath.section]]
+        let cellData = sectionData?[indexPath.row]
+        
+        cell.textLabel?.text = "\(cellData!)"
+        
+        return cell
     }
 
 
