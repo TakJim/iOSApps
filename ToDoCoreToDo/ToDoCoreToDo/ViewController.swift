74f9b399e97b73b6625586b7de7a40f653591403
TakJim <tak.jimbo@gmail.com>
2018-07-15 21:17:34 +0900

    アプリ制作開始

---
+++ ToDoCoreToDo/ToDoCoreToDo/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ToDoCoreToDo
+//
+//  Created by T. Jimbo on 2018/07/15.
+//  Copyright © 2018年 TakJim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: 
+    
+    @IBOutlet weak var taksTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
